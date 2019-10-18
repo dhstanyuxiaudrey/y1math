89a55132ef3cda7be33eff2f6a0d80af96d9f69a
dhstanyuxiaudrey <37992089+dhstanyuxiaudrey@users.noreply.github.com>
2019-10-18 14:40:25 +0800

    Update Audrey Tan Yu Xi 2E

--- t01primeshcflcm_primekth.py
+++ t01primeshcflcm_primekth.py
@@ -1,2 +1,2 @@
 # What is the kth prime number?
-
+A prime number is a natural number greater than 1 that cannot be formed by multiplying two smaller natural numbers. A natural number greater than 1 that is not prime is called a composite number
